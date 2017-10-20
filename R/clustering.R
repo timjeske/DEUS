e363a7ec476221d2c1835cf080b0426898c5c338
hastreiter <hastreiter@helmholtz-muenchen.de>
2017-10-20 14:13:40 +0200

    Alternative method for cluster parsing v2

--- R/clustering.R
+++ R/clustering.R
@@ -19,22 +19,22 @@ runClustering <- function(cdhit_path, sequences, out_dir, identityCutoff, length
   cluster.out <- system2(cdhit_path,command, stdout=TRUE)
 
   #Read in outfile and prepare for further processing
-  cluster.file <- paste(clusters,".clstr",sep="")
-  out<-readChar(cluster.file, file.info(cluster.file)$size)
-  cl.list<-strsplit(out,">Cluster ")
-  #Remove first empty element
-  cl.list[[1]]<-cl.list[[1]][-1]
-  cl.df<-data.frame(unlist(cl.list))
+#  cluster.file <- paste(clusters,".clstr",sep="")
+#  out<-readChar(cluster.file, file.info(cluster.file)$size)
+#  cl.list<-strsplit(out,">Cluster ")
+#  #Remove first empty element
+#  cl.list[[1]]<-cl.list[[1]][-1]
+#  cl.df<-data.frame(unlist(cl.list))
   cl.dir<-paste(out_dir, "Clusters",sep="/")
   dir.create(cl.dir, showWarnings = FALSE)
-  out.df<-(apply(cl.df,1,extractSequences,map=map,cl.dir=cl.dir))
-  out.df<-do.call("rbind",out.df)
+  out.df <- processClusters(map,out_dir)
+
+#  out.df<-(apply(cl.df,1,extractSequences,map=map,cl.dir=cl.dir))
+#  out.df<-do.call("rbind",out.df)
   out.df["sequences"] <- NULL
   names(out.df)[2]<-"ClusterID"
   return(out.df)
 }
-
-
 
 extractSequences<-function(entry,map,cl.dir){
   cl_id<-unlist(strsplit(entry,"\n"))[1]
@@ -45,7 +45,44 @@ extractSequences<-function(entry,map,cl.dir){
   sequences<-row.names(map)[map[,1]%in%qseqid]
   out=paste(">",qseqid,"\n",sequences,sep="")
   write.table(out,outfile,row.names=F,col.names=F,quote=F)
-
   out.df<-as.data.frame(cbind(qseqid,sequences,cl_id))
   return(out.df)
 }
+
+processClusters <- function(map,out_dir) {
+  cl.dir <- paste(out_dir, "Clusters",sep="/")
+  clusters <- paste(out_dir,"DESeq2_sig_sequences.cd_hit",sep="/")
+  cluster.file <- paste(clusters,".clstr",sep="")
+  con <- file(cluster.file, "r")
+  out.df <- data.frame()
+
+  while ( TRUE ) {
+    line <- readLines(con, n = 1)
+    if ( length(line) == 0 ) {
+      break
+    }
+    if(startsWith(line,">")){
+      cl_id <- unlist(strsplit(line,"\\s+",perl = T))[2]
+    }else{
+      seqIDsIndex <- regexpr("seq_[0-9]+",line,perl=TRUE)
+      qseqid <- c(regmatches(line, seqIDsIndex))
+      out.df <- rbind.data.frame(out.df,cbind(qseqid,cl_id))
+    }
+  }
+  close(con)
+
+  map$sequences<-row.names(map)
+  out.df<-merge(x = out.df, y = map, by.x = "qseqid",by.y="seq_id", all.x = TRUE)
+  names(out.df)
+  by(out.df,out.df$cl_id,printCluster,cl.dir=cl.dir)
+  return(out.df)
+}
+
+
+printCluster<-function(data,cl.dir){
+  cl_id<-data[1,2]
+  outfile <- paste(cl.dir,"/Cluster",cl_id,".fa",sep="")
+  out<-paste(">",data[,1],"\n",data[,3],sep="")
+  write.table(out,outfile,col.names=F,row.names=F,quote=F)
+}
+
