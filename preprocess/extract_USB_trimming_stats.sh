7c2649f59d94b777596f78a3418b3b768f1205a5
timjeske <tim.jeske@yahoo.de>
2018-03-23 12:07:36 +0100

    fix USB trimming stats

--- preprocess/extract_USB_trimming_stats.sh
+++ preprocess/extract_USB_trimming_stats.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# call: ./extract_USB_trimming_stats.sh ./trimming <fastq|fq> max_length
+# call: ./extract_USB_trimming_stats.sh ./trimming max_length
 
 dir=$1
-fq=$2
 ml=$3
 
 
 #collect quality metrics
 stats=$dir/"USB_trimming_stats.tsv"
-echo -e "sample\treads\tfrac_adapter\tremoved_shorter_16\tremoved_longer_${ml}\tfrac_quality\tremoved_shorter_16\treads_trimmed" > $stats
+echo -e "sample\treads\tfrac_adapter\tremoved_shorter_16\tremoved_longer_${ml}\tfrac_quality\treads_trimmed" > $stats
 
-for f in $dir/*${fq}.gz_trimming_report.txt; do
+for f in $dir/*fastq.gz_trimming_report.txt; do
   sample=$( basename $( grep "Input filename" $f | cut -d " " -f3 ) | cut -d "_" -f1 )
   rps=$( grep "Total reads processed" $f | rev | cut -d " " -f1 | rev )
   far=$( grep "Reads with adapters" $f | rev | cut -d " " -f1 | rev | tr -d '()' )
@@ -20,12 +19,11 @@ for f in $dir/*${fq}.gz_trimming_report.txt; do
   # open second report
   f2=${f/.fastq.gz/_trimmed.fq.gz}
   fqr=$( grep "Quality-trimmed: " $f2 | rev | cut -d " " -f1 | rev | tr -d '()' )
-  rm1=$( grep "Sequences removed because they became shorter than the length cutoff of 16 bp" $f2 | cut -f2 | cut -d " " -f1 )
+  #rm1=$( grep "Sequences removed because they became shorter than the length cutoff of 16 bp" $f2 | cut -f2 | cut -d " " -f1 )
   proc2=$( grep "sequences processed in total" $f2 | cut -d " " -f1 )
   reads_trimmed=$((proc2-rm1))
   printf "%s\t" $sample $rps $far >> $stats
   printf "%'d\t" $rm16 $rm49 >> $stats
   printf "%s\t" $fqr >> $stats
-  printf "%'d\t" $rm1 >> $stats
   printf "%'d\n" $reads_trimmed >> $stats 
 done 
