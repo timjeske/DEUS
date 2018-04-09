23003cb41a51b9fe5fd9aee71db0a1a0cd11f2df
timjeske <tim.jeske@yahoo.de>
2018-04-09 14:16:43 +0200

    less commands for clustering

--- sample_usage.R
+++ sample_usage.R
@@ -37,15 +37,10 @@ write.table(blastResult, paste(out_dir, "Sig_sequences.blastn.tsv",sep="/"), col
 
 # run clustering
 cd_hit <- "/storageNGS/ngs1/software/cdhit/cd-hit-est"
-seq_fasta <- paste(out_dir,"sig_sequences.fa",sep="/")
-write.table(sigSeqFasta,seq_fasta,quote = F,row.names = F,col.names = F)
-clustResult<-runClustering(cd_hit,seq_fasta,out_dir,0.9,0.9,9,map)
+clustResult<-runClustering(cd_hit, sigSeqFasta, out_dir, 0.9, 0.9, 9, map)
 
 # merge results
 classes <- c("mmu_piR","ENSMUST","tRNA","mmu-miR","retro")
 summary <- mergeResults(sigResults,countStats, blastResult, clustResult, map)
 summary <- addCountsOfFeatureClasses(summary, classes)
 writeSummaryFiles(summary,out_dir)
-
-# remove tmp files
-deleteTmp(out_dir)
