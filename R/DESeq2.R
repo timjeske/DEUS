016ed8caed70d36e3ddda883ae9f04f146f06cdd
timjeske <tim.jeske@yahoo.de>
2018-04-09 13:07:15 +0200

    add PCA

--- R/DESeq2.R
+++ R/DESeq2.R
@@ -21,6 +21,9 @@ runDESeq2 <- function(countData, phenoData, design, map, out_dir) {
   rld <- DESeq2::rlog(dds, blind=FALSE)
   plotSampleDistanceMap(rld,out_dir)
 
+  # plot PCA
+  plotPCA(rld,phenoData,out_dir)
+
   # plot MA
   pdf(paste(out_dir,"DESeq2_MAplot_shrunken.pdf",sep="/"), onefile=FALSE)
   DESeq2::plotMA(res, main = "DESeq2", ylim=c(-4,4))
@@ -50,5 +53,33 @@ plotSampleDistanceMap <- function(rld, out_dir) {
            clustering_distance_rows=sampleDists,
            clustering_distance_cols=sampleDists,
            col=colors)
+  dev.off()
+}
+
+#' Function to plot PCA
+#'
+#' @param rld regularized log transformed counts
+#' @param phenoData data frame with sample names as rownames and assigned condition in first column
+#' @param out_dir directory for sample distance map
+#' @keywords sample distance map
+#' @export
+#' @examples
+
+plotPCA <- function(rld, phenoData, out_dir) {
+  myPhenoData <- phenoData[, !(names(phenoData) %in% c("sample")), drop=FALSE]
+  pdf(paste(out_dir,paste(out_dir,"DESeq2_PCA.pdf",sep=""), sep="/"), onefile=FALSE)
+  if(ncol(myPhenoData) > 1) {
+    data <- plotPCA(rld, intgroup=colnames(myPhenoData), returnData=TRUE)
+    percentVar <- round(100 * attr(data, "percentVar"))
+    plt <- ggplot(data, aes(PC1, PC2, color=myPhenoData[,1], shape=myPhenoData[,2])) +
+      geom_point(size=3) +
+      xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+      ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+      labs(color=colnames(myPhenoData)[1]) +
+      labs(shape=colnames(myPhenoData)[2])
+  } else {
+    plt <- plotPCA(rld, intgroup=colnames(myPhenoData))
+  }
+  print(plt)
   dev.off()
 }
--- R/stats.R
+++ R/stats.R
@@ -54,5 +54,7 @@ getNoBlastHitFraction <- function(summary, countTable) {
   res <- colSums(sub)/colSums(countTable)
   res <- as.matrix(res)
   colnames(res) <- c("NA_fraction")
+  res$Sample <- row.names(res)
+  res <- res[,c(which(names(res)=="Sample"),which(names(res)!="Sample"))]
   return(res)
 }
