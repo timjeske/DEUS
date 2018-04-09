7cd7d0f97e5dadb493b297ecefd20369db86d491
timjeske <tim.jeske@yahoo.de>
2018-04-09 12:04:29 +0200

    fix map

--- sample_usage_getStats.R
+++ sample_usage_getStats.R
@@ -15,19 +15,19 @@ phenoInfo <- read.table(phenofile, header=T, row.names=1, check.names=FALSE)
 # create and filter count table, create sequence to sequenceID map
 countTable <- createCountTableFromFastQs(in_dir, phenoInfo=phenoInfo)
 countTable <- filterLowExp(countTable, phenoInfo)
-map <- createMap(countTable)
 
 # run DE analysis to normalize count data
 design <- ~ 1
-deResults <- runDESeq2(countDataFilt, phenoInfo, design, map, out_dir)
-sigResults <- deResults$deResult
-sigSeqFasta <- sequencesAsFasta(sigResults,map)
+deResults <- runDESeq2(countTable, phenoInfo, design, map, out_dir)
+normCts <- deResults$normCounts
+map <- createMap(normCts)
+seqFasta <- sequencesAsFasta(normCts,map)
 
 # run blast
 blast_exec <- "/storageNGS/ngs1/software/ncbi-blast-2.6.0+/bin/blastn"
 blast_db <-"/storageNGS/ngs1/software/ncbi-blast-2.6.0+/blastdb/MouseDB_piRNABank.fa"
 ncores <- 16
-blastResult <- runBlast(blast_exec, blast_db, ncores, sigSeqFasta, identity = 95)
+blastResult <- runBlast(blast_exec, blast_db, ncores, seqFasta, identity = 95)
 write.table(blastResult, paste(out_dir, "Sig_sequences.blastn.tsv",sep="/"), col.names=T, quote=F, sep="\t", row.names=F)
 
 # merge results
@@ -38,6 +38,6 @@ writeSummaryFiles(summary,out_dir)
 
 generateSummaryPlots(summary,classes, out_dir)
 
-res <- getNoBlastHitFraction(summary, deResults$normCounts)
+res <- getNoBlastHitFraction(summary, normCts)
 write.table(res,paste(out_dir,"NA_fraction_per_sample.tsv",sep="/"),row.names = T, col.names = T, quote=F, sep="\t")
 
